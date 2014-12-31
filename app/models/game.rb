2c2c022f7a54e138440ddae4465ef74e0a76e37c
gustav.rodewald <gustav.rodewald@asurion.com>
2014-12-31 12:22:59 -0600

    Basic display of the board

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,13 @@
 class Game < ActiveRecord::Base
+  @@columns = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+  @@rows = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+  def Columns
+    @@columns
+  end
+
+  def Rows
+    @@rows
+  end
+  
 end
